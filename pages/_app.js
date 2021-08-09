06d68dd5f79d0cdcd769b1761cc31042eeb18cb9
germmand <german.aguilera17@gmail.com>
2021-08-09 10:08:20 -0400

    Initial commit.

---
+++ pages/_app.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Head from "next/head";
+import Router from "next/router";
+
+import PageChange from "components/PageChange/PageChange.js";
+
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/nextjs-argon-dashboard.scss";
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  document.body.classList.add("body-page-transition");
+  ReactDOM.render(
+    <PageChange path={url} />,
+    document.getElementById("page-transition")
+  );
+});
+Router.events.on("routeChangeComplete", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+Router.events.on("routeChangeError", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>NextJS Argon Dashboard by Creative Tim</title>
+        <script src="https://maps.googleapis.com/maps/api/js?key="></script>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Hydrate>
+      </QueryClientProvider>
+    </React.Fragment>
+  );
+}
